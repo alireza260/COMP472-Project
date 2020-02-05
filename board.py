79a1816505f0bb38be229fdfa0af2689d93121f2
moumaa <31826851+moumaa@users.noreply.github.com>
2020-02-05 16:18:12 -0500

    fix adjacent cell list order

--- board.py
+++ board.py
@@ -27,8 +27,8 @@ def adjacent_nodes(y, x):
 
     adjacency_list = []
 
-    if y + 1 <= boardSize-1:
-        adjacency_list.append((y+1, x))
+    if x - 1 >= 0:
+        adjacency_list.append((y, x-1))
 
     if x + 1 <= boardSize-1:
         adjacency_list.append((y, x+1))
@@ -36,8 +36,8 @@ def adjacent_nodes(y, x):
     if y - 1 >= 0:
         adjacency_list.append((y-1, x))
 
-    if x - 1 >= 0:
-        adjacency_list.append((y, x-1))
+    if y + 1 <= boardSize-1:
+        adjacency_list.append((y+1, x))
 
     return adjacency_list
 
