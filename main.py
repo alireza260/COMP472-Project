79fa849bc699529daca1ad632df0ee8e9fa626d3
Philip Kyres <pkyres@gmail.com>
2020-02-09 18:42:58 -0500

    Fixed 2 bugs. Simplified the algo a bit. Added total_moves_tried counter

--- main.py
+++ main.py
@@ -16,6 +16,8 @@ og_board = np.random.randint(2, size=(n, n))
 print("dfs max depth:")
 max_d = int(input())
 
+total_moves_tried = 0
+
 
 def print_matrix(matrix):
     for arr in matrix:
@@ -26,6 +28,9 @@ def print_matrix(matrix):
 
 
 def flip(board, move_history, row_index, col):
+    global total_moves_tried
+    total_moves_tried += 1
+
     # Deep copy matrices
     move_history_copy = [[tile for tile in row] for row in move_history]
     board_copy = [[tile for tile in row] for row in board]
@@ -84,49 +89,57 @@ def determine_best(old, new):
                     return new
 
 
-def recur_dfs(board, move_history, row_index, col_index, depth):
+def recur_dfs(board, move_history, depth):
     """
     Returns None if no solution was found, or a tuple with:
     - the depth (i.e. number of moves + 1)
     - the solution steps: an array of tuples with: row, col, board matrix
     """
-    # Check board bounds and depth and move history
-    if row_index >= n or col_index >= n or depth > max_d or move_history[row_index][col_index]:  # "max_d is included"
-        return None
-
-    new_board, new_move_history = flip(board, move_history, row_index, col_index)
-
     # Check for completion
-    if not any(1 in row for row in new_board):
-        return depth, [(row_index, col_index, new_board)]
+    if not any(1 in row for row in board):
+        return depth, []
+
+    if depth == max_d:  # "max_d is included"
+        return None
 
     best = None
 
     # Brute force recur to every spot on board
-    for r_index, row in enumerate(new_board):
-        for c_index, tile in enumerate(row):
-            new = recur_dfs(new_board, new_move_history, r_index, c_index, depth + 1)
+    for row_index in range(n):
+        for col_index in range(n):
+            # Check move history
+            if move_history[row_index][col_index]:
+                continue
+
+            new_board, new_move_history = flip(board, move_history, row_index, col_index)
+
+            new = recur_dfs(new_board, new_move_history, depth + 1)
+
+            if new is not None:
+                # Add move to solution path
+                new[1].append((row_index, col_index, new_board))
+
+            # Determine if this move is better than the other moves so far
             best = determine_best(best, new)
 
     if best is None:
         return None
 
-    # Add move to history
-    best[1].append((row_index, col_index, new_board))
     return best
 
 
 def dfs(board):
     empty_history = [x[:] for x in [[False] * n] * n]
-    return recur_dfs(board, empty_history, 0, 0, 1)  # "root = 1"
+    return recur_dfs(board, empty_history, 1)  # "root = 1"
 
-
-print_matrix(og_board)  # TODO delete
 
 start = time.time()
 result = dfs(og_board)
 end = time.time()
 print("Seconds elapsed: " + str(end - start))
+print("Moves tried: " + str(total_moves_tried))
+
+print_matrix(og_board)
 
 if result is None:
     print("No solution")
