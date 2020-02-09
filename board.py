f8633599ed2d5fbb7ec26fbd8098f1dea5612f3d
moumaa <31826851+moumaa@users.noreply.github.com>
2020-02-09 18:59:42 -0500

    merge Philip's solution to get final algorithm working

--- board.py
+++ board.py
@@ -1,4 +1,5 @@
 import numpy as np
+import time
 
 closed_list = []
 open_list = []
@@ -15,9 +16,6 @@ with open("input.txt") as input:
 
 one_d_array = [c for c in str(arrayInput)]
 
-
-
-
 if boardSize<3:
     boardSize=3
     print("board size adjusted to 3")
@@ -33,125 +31,134 @@ random_matrix_array = np.reshape(one_d_array, (-1, boardSize)).astype(int)
 #change array to random instead of getting it from input file
 #random_matrix_array = np.random.randint(2,size=(boardSize,boardSize))
 
+total_moves_tried = 0
+
+number_of_cells = boardSize*boardSize
+
 def print_array(random_matrix_array):
 
     for a in random_matrix_array:
         for elem in a:
             print("{}".format(elem).rjust(3), end="")
         print(end="\n")
+    print(end="\n")
+
+def flip(board, move_history, row_index, col):
+
+    global total_moves_tried
+    total_moves_tried += 1
+
+    # Deep copy matrices
+    move_history_copy = [[tile for tile in row] for row in move_history]
+    board_copy = [[tile for tile in row] for row in board]
+
+    move_history_copy[row_index][col] = True
+    board_copy[row_index][col] ^= 1
+
+    if col - 1 >= 0:
+        board_copy[row_index][col - 1] ^= 1
+
+    if col + 1 < boardSize:
+        board_copy[row_index][col + 1] ^= 1
+
+    if row_index - 1 >= 0:
+        board_copy[row_index - 1][col] ^= 1
+
+    if row_index + 1 < boardSize:
+        board_copy[row_index + 1][col] ^= 1
+
+    return board_copy, move_history_copy
+
+def get_next_tile(row, col):
+    if col + 1 >= boardSize:
+        row += 1
+        col = 0
+    else:
+        col += 1
+
+    return row, col
+
+def determine_best(old, new):
+    if old is None:
+        return new
+
+    if new is None:
+        return old
+
+    # "the goal is to find the smallest number of moves to bring the board to its goal configuration"
+    if old[0] < new[0]:
+        return old
+    if new[0] < old[0]:
+        return new
+
+    # Prefer "the board that has its first white token(s) at an earlier position based on left-to-right, then top-down order"
+    for t1, t2 in zip(old[1], new[1]):  # Iterate through each move
+        b1 = t1[2]
+        b2 = t2[2]
+
+        for arr1, arr2 in zip(b1, b2):  # Iterate through each row
+            for ele1, ele2 in zip(arr1, arr2):  # Iterate through each tile
+                if ele1 < ele2:
+                    return old
+                if ele2 < ele1:
+                    return new
+
+def recur_dfs(board, move_history, depth):
+    """
+    Returns None if no solution was found, or a tuple with:
+    - the depth (i.e. number of moves + 1)
+    - the solution steps: an array of tuples with: row, col, board matrix
+    """
+    # Check for completion
+    if not any(1 in row for row in board):
+        return depth, []
+
+    if depth == max_d:  # "max_d is included"
+        return None
+
+    best = None
+
+    # Brute force recur to every spot on board
+    for row_index in range(boardSize):
+        for col_index in range(boardSize):
+            # Check move history
+            if move_history[row_index][col_index]:
+                continue
+
+            new_board, new_move_history = flip(board, move_history, row_index, col_index)
+
+            new = recur_dfs(new_board, new_move_history, depth + 1)
+
+            if new is not None:
+                # Add move to solution path
+                new[1].append((row_index, col_index, new_board))
+
+            # Determine if this move is better than the other moves so far
+            best = determine_best(best, new)
+
+    if best is None:
+        return None
+
+    return best
 
 
-def adjacent_nodes(y, x):
+def dfs(board):
+    empty_history = [x[:] for x in [[False] * boardSize] * boardSize]
+    return recur_dfs(board, empty_history, 1)  # "root = 1"
 
-    adjacency_list = []
-
-    if x - 1 >= 0:
-        adjacency_list.append((y, x-1))
-
-    if x + 1 <= boardSize-1:
-        adjacency_list.append((y, x+1))
-
-    if y - 1 >= 0:
-        adjacency_list.append((y-1, x))
-
-    if y + 1 <= boardSize-1:
-        adjacency_list.append((y+1, x))
-
-    return adjacency_list
-
-def closed_cells(y,x):
-
-    closed_list.append((y,x)) if (y,x) not in closed_list else closed_list
-
-    return closed_list
-
+start = time.time()
+result = dfs(random_matrix_array)
+end = time.time()
+print("Seconds elapsed: " + str(end - start))
+print("Moves tried: " + str(total_moves_tried))
 
 print_array(random_matrix_array)
 
-number_of_cells = boardSize*boardSize
-
-def open_cells(adjacency_list):
-
-    open_list = [x for x in adjacency_list if x not in closed_list]
-
-    return open_list
-
-def cells_available_history(adjacency_list):
-
-    available_history_list.append(adjacency_list)
-
-    return available_history_list
-
-
-def flip_cell(y, x):
-
-    random_matrix_array[y,x] -=1
-
-    if y + 1 <= boardSize-1:
-        random_matrix_array[y+1, x] -= 1
-
-    if y - 1 >= 0:
-        random_matrix_array[y-1, x] -= 1
-
-    if x + 1 <= boardSize-1:
-        random_matrix_array[y, x+1] -= 1
-
-    if x - 1 >= 0:
-        random_matrix_array[y, x-1] -= 1
-
-    random_matrix_array[random_matrix_array < 0] = 1
-
-    print_array(random_matrix_array)
-
-def dfs():
-
-    count_white_cells = 0
-
-    next_cell_y = 0
-    next_cell_x = 0
-
-    y = 0
-    x = 0
-
-    while count_white_cells != number_of_cells and open_cells(adjacent_nodes(y, x)):
-        print_array("-")
-
-        flip_cell(next_cell_y,next_cell_x)
-
-        count_white_cells = boardSize*boardSize - np.count_nonzero(random_matrix_array)
-
-        print("adjacency cells: ")
-        print(adjacent_nodes(y, x))
-
-        print("visited cells: ")
-        print(closed_cells(y,x))
-
-        print("available cells: ")
-        print(open_cells(adjacent_nodes(y, x)))
-
-        print("cells history:")
-        print(cells_available_history(adjacent_nodes(y, x)))
-
-        print("white cells ratio: ")
-        print(count_white_cells , "/" , number_of_cells)
-
-        next_cell = open_cells(adjacent_nodes(y, x))[0]
-
-        next_cell_y = int(str(next_cell)[1])
-
-        next_cell_x = int(str(next_cell)[4])
-
-        y = next_cell_y
-
-        x = next_cell_x
-
-        print("next y:",next_cell_y, "next x:",next_cell_x)
-
-    if not open_cells(adjacent_nodes(y, x)):
-        print("There is no solution.")
-
-    elif count_white_cells == number_of_cells:
-        print("Congratulations you have won!")
-
-dfs()
+if result is None:
+    print("No solution")
+else:
+    result_depth, result_steps = result
+    print("Final depth: " + str(result_depth))
+    for i in reversed(result_steps):
+        print_array(i[2])
 
