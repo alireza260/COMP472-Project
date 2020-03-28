e7081449977024395b7002a6b08488e1b3b22280
moumaa <31826851+moumaa@users.noreply.github.com>
2020-03-28 18:36:41 -0400

    probability progress

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import numpy as np
 from nltk.tokenize import word_tokenize
 import string
 import re
@@ -57,10 +58,51 @@ with open('training-tweets.txt', encoding="utf8") as training_file:
         elif trunc_at_language(message, "	", 2) == "pt":
             portugese_v.extend(pre_process_tweets(trunc_at_tweet(message, "	", 3)))
 
+def calc_language_prob(vocabulary, t_element):
+
+    return np.log10((vocabulary.count(t_element)) / len(vocabulary))
+
+with open('test-tweets-given.txt', encoding="utf8") as testing_file:
+    for line in testing_file:
+        correct_l = trunc_at_language(str(line), "	", 2)
+        message = pre_process_tweets(trunc_at_tweet(str(line), "	", 3))
+
+        basque_prob = catalan_prob = galician_prob = spanish_prob = english_prob = portugese_prob = 0
+
+        for x in message:
+            basque_prob += calc_language_prob(basque_v, x)
+            catalan_prob += calc_language_prob(catalan_v, x)
+            galician_prob += calc_language_prob(galician_v, x)
+            spanish_prob += calc_language_prob(spanish_v, x)
+            english_prob += calc_language_prob(english_v, x)
+            portugese_prob += calc_language_prob(portugese_v, x)
+
+        most_probable_l = max(basque_prob,catalan_prob,galician_prob,spanish_prob,english_prob,portugese_prob)
+
+        if most_probable_l == basque_prob:
+            print(correct_l, ": eu")
+        elif most_probable_l == catalan_prob:
+            print(correct_l, ": ca")
+        elif most_probable_l == galician_prob:
+            print(correct_l, ": gl")
+        elif most_probable_l == spanish_prob:
+            print(correct_l, ": es")
+        elif most_probable_l == english_prob:
+            print(correct_l, ": en")
+        elif most_probable_l == portugese_prob:
+            print(correct_l, ": pt")
+
+
+
+
+
+
+
+
 
 #print("basque vocabulary: ",basque_v)
 #print("catalan vocabulary: ", catalan_v)
 #print("galician vocabulary: ", galician_v)
 #print("spanish vocabulary: ", spanish_v)
-print("english vocabulary: ", english_v)
+#print("english vocabulary: ", english_v)
 #print("portugese vocabulary: ", portugese_v)
